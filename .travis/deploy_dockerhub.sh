ae592dd9cc12302233927bcdbb6dd05aa47216fb
George Sykes <georgesykes86@gmail.com>
2018-06-07 18:01:51 +0100

    Adds updated docker travis integration script

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+docker login -u $DOCKER_USER -p $DOCKER_PASS
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     TAG="latest"
 else
     TAG="$TRAVIS_BRANCH"
 fi
-docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
+SLUGNAME= echo $TRAVIS_REPO_SLUG | tr '[:upper:]' '[:lower:]'
+docker build -t $TRAVIS_REPO_SLUG:$TAG .
 docker push $TRAVIS_REPO_SLUG
