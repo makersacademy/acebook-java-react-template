3d4a2992b741e6cf05934a610c8bf9f88bda9c82
John Newman <newmanj903@gmail.com>
2018-06-07 11:27:00 +0100

    trying out jest

---
+++ src/main/js/__tests__/test-test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { shallow, mount, render } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Posts from '../posts/posts';
+import { configure } from 'enzyme';
+import renderer from 'react-test-renderer';
+
+configure({ adapter: new Adapter() });
+
+describe('a test', function(){
+    it('testing posts', function(){
+      const tree = renderer.create(
+        <Posts />
+      ).toJSON();
+      expect(tree).toMatchSnapshot();
+    })
+})
