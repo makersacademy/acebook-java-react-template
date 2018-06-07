f63aa5e237a14fb0a45e021e367a4d33de3240fd
William Powell <william.powell92@gmail.com>
2018-06-07 15:22:27 +0100

    added code to check for port environment variable for heroku deployment

--- Dockerfile
+++ Dockerfile
@@ -17,4 +17,4 @@ ARG JAR_FILE=target/acebook-template-1.0-SNAPSHOT.jar
 ADD ${JAR_FILE} acebook-template.jar
 
 # Run the jar file
-ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-jar","/acebook-template.jar"]
+CMD ["java","-Djava.security.egd=file:/dev/./urandom","-jar","/acebook-template.jar"]
--- src/main/java/com/millieandco/acebook/Application.java
+++ src/main/java/com/millieandco/acebook/Application.java
@@ -7,6 +7,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
+
+        String ENV_PORT = System.getenv().get("PORT");
+        String ENV_DYNO = System.getenv().get("DYNO");
+        if(ENV_PORT != null && ENV_DYNO != null) {
+            System.getProperties().put("server.port", ENV_PORT);
+        }
+
         SpringApplication.run(Application.class, args);
     }
 }
+
