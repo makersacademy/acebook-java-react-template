677924b9ce1b368204c8c73f9864431052a0b8cd
William Powell <william.powell92@gmail.com>
2018-06-07 18:09:52 +0100

    added docker login to deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
+docker login --username=_ --password=$HEROKU_API registry.heroku.com
 heroku container:push web --app acebook-millieandco
 heroku container:release web --app acebook-millieandco
