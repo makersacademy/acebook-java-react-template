d090e4c891d33df89b1fff841e8ab7d0b06833bc
William Powell <william.powell92@gmail.com>
2018-06-08 14:22:19 +0100

    added mvn clean install

--- scripts/deploy_heroku.sh
+++ scripts/deploy_heroku.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+mvn clean install
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 heroku plugins:install heroku-container-registry
 docker login --username=_ --password=$HEROKU_API_KEY registry.heroku.com
