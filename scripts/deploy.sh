e2a32f0df0d4686984afa95516bf6ec7bdbc1d01
William Powell <william.powell92@gmail.com>
2018-06-08 10:24:02 +0100

    changed deploy code

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,9 @@
-#!/bin/bash
-docker login --username=_ --password=$HEROKU_API registry.heroku.com
+#!/bin/sh
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+heroku plugins:install heroku-container-registry
+heroku login
+william.powell92@gmail.com
+Herokupassword1;
+heroku container:login
 heroku container:push web --app acebook-millieandco
 heroku container:release web --app acebook-millieandco
