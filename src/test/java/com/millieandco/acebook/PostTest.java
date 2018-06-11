16410238b5910602fa6720b007ffc993072cbb8e
William Powell <william.powell92@gmail.com>
2018-06-11 14:35:15 +0100

    added unit tests for post

--- src/test/java/com/millieandco/acebook/PostTest.java
+++ src/test/java/com/millieandco/acebook/PostTest.java
@@ -10,9 +10,18 @@ public class PostTest {
 
 	private Post post = new Post("hello");
 
-//	@Test
-//	public void postHasContent() {
-//		assertThat(post.getContent(), containsString("hello"));
-//	}
+	@Test
+	public void postHasId() {
+		assertEquals(post.getId(), 0);
+	}
 
+	@Test
+	public void postHasContent() {
+		assertEquals(post.getComment(), "hello");
+	}
+
+	@Test
+	public void toStringReturnsCorrectString() {
+		assertEquals(post.toString(), "Post[id=0, comment='hello']");
+	}
 }
