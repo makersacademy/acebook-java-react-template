0924567bf97090920488433cf0b33df394be68ef
William Powell <william.powell92@gmail.com>
2018-06-11 11:36:24 +0100

    uncommented out DatabaseLoader.java

--- src/main/java/com/millieandco/acebook/DatabaseLoader.java
+++ src/main/java/com/millieandco/acebook/DatabaseLoader.java
@@ -1,22 +1,21 @@
-//package com.millieandco.acebook;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.CommandLineRunner;
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//public class  DatabaseLoader implements CommandLineRunner {
-//  private final PostRepository repository;
-//
-//  @Autowired
-//  public DatabaseLoader(PostRepository repository) {
-//    this.repository = repository;
-//  }
-//
-//  @Override
-//  public void run(String... strings) throws Exception {
-//    this.repository.save(new Post("Hey, folks! Welcome to Acebook!"));
-//  }
-//}
-//
+package com.millieandco.acebook;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.stereotype.Component;
+
+@Component
+public class  DatabaseLoader implements CommandLineRunner {
+  private final PostRepository repository;
+
+  @Autowired
+  public DatabaseLoader(PostRepository repository) {
+    this.repository = repository;
+  }
+
+  @Override
+  public void run(String... strings) throws Exception {
+    this.repository.save(new Post("Hey, folks! Welcome to Acebook!"));
+  }
+}
