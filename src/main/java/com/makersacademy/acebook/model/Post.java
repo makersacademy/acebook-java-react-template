f3247de4398d08e2b9ce64ad3bfecbaf9cf6dc60
Edward Withers <dearshrewdwit@gmail.com>
2018-06-11 17:26:38 +0100

    add dev db

--- src/main/java/com/makersacademy/acebook/model/Post.java
+++ src/main/java/com/makersacademy/acebook/model/Post.java
@@ -3,14 +3,19 @@ package com.makersacademy.acebook.model;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.GenerationType;
 
 import lombok.Data;
 
 @Data
 @Entity
+@Table(name = "POSTS")
 public class Post {
 
-    private @Id @GeneratedValue Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String content;
 
     private Post() {}
