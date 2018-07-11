ad35a1ae2596bd01d11ba877e683bb19064e03b9
Benjamin Tomkins <bentomkins@hotmail.com>
2018-07-11 12:08:21 +0100

    hello world controller tests

---
+++ src/test/java/com/makersacademy/acebook/_helloworld/HelloWorldTest.java
@@ -0,0 +1,44 @@
+package com.makersacademy.acebook._helloworld;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.junit.runner.RunWith;
+import org.junit.Test;
+
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HelloWorldTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnHelloWorldMessage() throws Exception {
+        this.mockMvc.perform(get("/hello-world"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello World")));
+    }
+
+    @Test
+    public void shouldReturnHelloPathVariable() throws Exception {
+        this.mockMvc.perform(get("/hello-world/Ben"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello, Ben")));
+    }
+
+}
+
