b9221219e6cffe59ab8932096ebefb9a96572ccc
Laura Petre <laura.d.petre@gmail.com>
2018-07-11 12:27:14 +0100

    Added placeholder image to posts

--- src/main/js/posts/post.js
+++ src/main/js/posts/post.js
@@ -3,9 +3,17 @@ import React from 'react';
 const Post = (props) => {
 	return (
 		<div className='post-main'>
-			<div className='post-content'>
-				{props.post.content}
-			</div>
+		    <div className='image'>
+		        <img src="/user-placeholder.png"  height="100" width="100"/>
+		    </div>
+		    <div className='content-timestamp'>
+                <div className='post-content'>
+                    {props.post.content}
+                </div>
+                <div className='post-timestamp'>
+                    {props.post.timestamp}
+                 </div>
+            </div>
 		</div>
 	)
 }
