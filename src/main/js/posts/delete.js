0766b0ead60ac9988ada1e2958be51ec8ef3b86f
Damion Hardy <damionhardy@Damos-MacBook-Pro-8.local>
2018-10-29 17:01:07 +0000

    delete btn

--- src/main/js/posts/delete.js
+++ src/main/js/posts/delete.js
@@ -1,19 +1,29 @@
 import React from 'react';
+import axios from 'axios';
 
 class Delete extends React.Component {
   constructor(props) {
     super(props);
-    
+
   }
 
   delete() {
+      console.log(this.props.post._links.post.href);
+      var urlPath = this.props.post._links.post.href;
+      var deleteObject = {
+        method: 'delete',
+        url: urlPath
+      }
+
+      axios(deleteObject);
 
   }
 
 	render() {
+    console.log(this.props);
 		return (
       <div>
-      <button click={() => this.delete()}>Delete</button>
+      <button onClick={() => this.delete()}>Delete</button>
       </div>
 		)
 	}
--- src/main/js/posts/post.js
+++ src/main/js/posts/post.js
@@ -3,10 +3,11 @@ import Delete from './delete.js'
 import Update from './update.js'
 
 const Post = (props) => {
-
+	console.log(props);
+	// var key = get digit of props.post._links.href
 	return (
 		<div className='post-main'>
-			<Delete key={props.key} />
+			<Delete post={props.post} />
 			<Update content={props.post.content} key={props.key} />
 			<div className='post-content'>
 				{props.post.content}
