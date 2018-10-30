7abf9dccc8fe8a516d442f9aa0c1f4a1754e93b7
shweta patil <shwetapatil@shwetas-MacBook-Pro.local>
2018-10-30 14:43:31 +0000

    updates databse model files

--- src/main/java/com/makersacademy/acebook/model/Like.java
+++ src/main/java/com/makersacademy/acebook/model/Like.java
@@ -8,30 +8,23 @@ import javax.persistence.GenerationType;
 
 import lombok.Data;
 
-import java.sql.Timestamp;
-
 @Data
 @Entity
-@Table(name = "COMMENTS")
-public class Comment {
+@Table(name = "LIKES")
+public class Like {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private int user_id;
     private int post_id;
-    private String content_comment;
-    private Timestamp comment_date_time;
 
+    private Like() {}
 
-    private Comment() {}
-
-    public Comment(Long id, int user_id, int post_id, String content_comment, Timestamp comment_date_time) {
+    public Like(Long id, int user_id, int post_id) {
         this.id = id;
         this.user_id = user_id;
         this.post_id = post_id;
-        this.content_comment = content_comment;
-        this.comment_date_time = comment_date_time;
     }
 
 }
---
+++ src/main/java/com/makersacademy/acebook/repository/LikeRepository.java
@@ -0,0 +1,8 @@
+package com.makersacademy.acebook.repository;
+
+import com.makersacademy.acebook.model.Like;
+import org.springframework.data.repository.CrudRepository;
+
+public interface LikeRepository extends CrudRepository<Like, Long> {
+
+}
