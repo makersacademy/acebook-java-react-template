c0784cbf9deefb6ac0eaf0e6ef96096e8fd6fa07
shweta patil <shwetapatil@shwetas-MacBook-Pro.local>
2018-10-30 11:47:36 +0000

    adds user table

---
+++ src/main/java/com/makersacademy/acebook/model/User.java
@@ -0,0 +1,32 @@
+package com.makersacademy.acebook.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.GenerationType;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "USERS")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String password;
+    private String name;
+
+    private User() {}
+
+    public User(Long id, String email, String password, String name) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.name = name;
+    }
+
+}
--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,6 +1,14 @@
 DROP TABLE IF EXISTS posts;
+DROP TABLE IF EXISTS users;
 
 CREATE TABLE posts (
   id bigserial PRIMARY KEY,
   content varchar(250) NOT NULL
+);
+
+CREATE TABLE users (
+  id bigserial PRIMARY KEY,
+  email varchar (80) NOT NULL,
+  password varchar (12) NOT NULL,
+  name varchar (20) NOT NULL
 );
