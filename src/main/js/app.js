f32204134f2495086f816dc7c35646f3c230ff50
AlexDresco <alexandra.d1510@gmail.com>
2018-10-31 17:04:28 +0000

    Login react page done

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -3,12 +3,15 @@ const ReactDOM = require('react-dom');
 
 import PostsBuilder from './posts/postsBuilder'
 import InputPost from './posts/inputPost'
+import SignUp from './users/signUp'
 
 class App extends React.Component {
 
   render() {
     return (
         <div>
+            <SignUp />
+            {/*<LogIn />*/}
             <InputPost />
             <PostsBuilder />
         </div>
---
+++ src/main/js/users/signUp.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+
+class SignUp extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className='signup_section'>
+                <h1 className='sign_up_header'>
+                    Please enter your details
+                </h1>
+                <form>
+                    <input type="email" name="email" placeholder="example@hello.com"/><br />
+                    <input type="password" name="password" placeholder="XXXXXX"/><br />
+                    <input type="text" name="name" placeholder="Xy Zello"/><br />
+                    <button type="submit" name="sign_up">Sign up</button>
+                </form>
+            </div>
+        )
+    }
+
+}
+
+
+
+export default SignUp;
+
