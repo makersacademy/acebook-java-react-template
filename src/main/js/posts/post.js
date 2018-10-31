177ce5f118e5fb484cdc462f0f10fecd2ce3897c
Vu Le <core.safecrlf>
2018-10-31 20:17:56 +0000

    able to now delete posts from frontend - reflects on backend - add some styling

--- src/main/js/posts/post.js
+++ src/main/js/posts/post.js
@@ -4,7 +4,24 @@ import Comment from './comment'
 class Post extends React.Component {
     constructor(props) {
     super(props);
+    this.onDelete = this.onDelete.bind(this)
     }
+
+  onDelete(post) {
+     console.log("delete is being called *** " + this.props.post._links.self.href)
+     fetch(this.props.post._links.self.href, {
+        method: "DELETE",
+
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+      }).then(response => {
+          this.props.setpost();
+          console.log("delete successful");
+      })
+
+  }
 
     render(){
     let date = new Date(this.props.post.createdAt).toString();
@@ -15,6 +32,10 @@ class Post extends React.Component {
             </div>
             <div>{date}</div>
             <div><Comment post={this.props.post} setpost = {this.props.setpost}/></div>
+            <div className="delete-btn">
+                <br />
+                <button onClick={this.onDelete} className="del-btn">Delete this post     <i className="fas fa-trash-alt"></i></button>
+            </div>
         </div>
         )
     }
