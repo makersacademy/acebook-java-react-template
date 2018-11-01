a59f4f4d4aa97d178768f44af1ddce38e973d507
shweta patil <shwetapatil@shwetas-MacBook-Pro.local>
2018-11-01 14:00:32 +0000

    cleaning files

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -6,25 +6,15 @@ import { BrowserRouter } from 'react-router-dom'
 import Header from './header'
 import Main from './main'
 
-import PostsBuilder from './posts/postsBuilder'
-import InputPost from './posts/inputPost'
-import SignUp from './users/signUp'
-import LogIn from './users/login'
-
 // this component will be rendered by our <___Router>
 class App extends React.Component {
 
   render() {
-    return (
-        <div>
-            {/*<SignUp />*/}
-            {/*<LogIn />*/}
-            {/*<InputPost />*/}
-            {/*<PostsBuilder />*/}
-            <Header />
-            <Main />
-
-        </div>
+      return (
+          <div>
+              <Header />
+              <Main />
+          </div>
     )
   }
 }
