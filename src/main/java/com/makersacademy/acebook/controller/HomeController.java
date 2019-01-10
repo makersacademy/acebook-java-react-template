06400367d74835aa6f5964603790a8a3988d9bd6
lindsey <lindsey@makers.tech>
2019-01-10 18:56:45 +0000

    Add spring templates

--- src/main/java/com/makersacademy/acebook/controller/HomeController.java
+++ src/main/java/com/makersacademy/acebook/controller/HomeController.java
@@ -1,14 +1,35 @@
 package com.makersacademy.acebook.controller;
 
+import com.makersacademy.acebook.model.Post;
+import com.makersacademy.acebook.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HomeController {
+
+	private final PostRepository postRepository;
+
+	@Autowired
+	public HomeController(PostRepository postRepository) {
+		this.postRepository = postRepository;
+	}
 
 	@RequestMapping(value = "/")
 	public String index() {
 		return "index";
 	}
+
+	@RequestMapping(value = "/greeting")
+	@GetMapping("/greeting")
+	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+
+		return "greeting";
+	}
+
+
 
 }
