385f8f3663b323e95453be698b534c378835eb3e
JRG <47917431+JR-G@users.noreply.github.com>
2020-09-28 20:25:06 +0100

    Styling: Formatting the posts on the home screen

--- src/main/js/posts/post.js
+++ src/main/js/posts/post.js
@@ -4,14 +4,26 @@ const Post = (props) => {
 	return (
 		<div className='post-main'>
 
-			<div className='post-name'>
-				{props.post.name}
+			<div className='post-avatar'>
+				<img src="https://vignette.wikia.nocookie.net/lotr/images/e/e7/Gandalf_the_Grey.jpg/revision/latest/top-crop/width/360/height/450?cb=20121110131754"/>
 			</div>
-			<div className='post-content'>
-				{props.post.content}
-			</div>
-			<div className='post-time'>
-				{props.post.post_time}
+			<div className='post-box'>
+				<div className='post-content'>
+					{props.post.content}
+				</div>
+				<div className='post-footer'>
+					<div className='post-info'>
+						<span className='post-name'>
+							{props.post.name}
+						</span>
+						<span className='post-time'>
+							{props.post.post_time}
+						</span>
+					</div>
+				</div>
+				{/*<div className="post-actions">*/}
+				{/*	<a href="#">Reply</a>*/}
+				{/*</div>*/}
 			</div>
 		</div>
 
