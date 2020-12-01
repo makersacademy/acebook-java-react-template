1b454033511989e35fe5c78f3d561f387362a180
keowashere <michaelkeohane@live.com>
2020-12-01 10:30:25 +0000

    test

--- src/main/java/com/makersacademy/acebook/repository/PostRepository.java
+++ src/main/java/com/makersacademy/acebook/repository/PostRepository.java
@@ -1,8 +1,11 @@
 package com.makersacademy.acebook.repository;
 
 import com.makersacademy.acebook.model.Post;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostRepository extends CrudRepository<Post, Long> {
+
+
+public interface PostRepository extends JpaRepository<Post, Long> {
 
 }
+
---
+++ src/test/java/com/makersacademy/acebook/controller/UserControllerTest.java
@@ -0,0 +1,4 @@
+package com.makersacademy.acebook.controller;
+
+public class UserControllerTest {
+}
