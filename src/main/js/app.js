931030047a1f6cd4b1815e684871104d7687787f
Arran Oakes <arran.oakes@gmail.com>
2021-07-16 10:56:54 +0100

    added dummy form js - not displaying on app yet

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -1,13 +1,17 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 
-import PostsBuilder from './posts/postsBuilder'
+import PostsBuilder from './posts/postsBuilder';
+import postForm from '.form/postForm';
 
 class App extends React.Component {
 
   render() {
     return (
+      <div className="App">
+      <postForm />
       <PostsBuilder />
+      </div>
     )
   }
 }
