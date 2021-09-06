eb49cad9c7a8549fc24fbde8261687b94b9ef1d0
NazirShaikh <shain003.212@gmail.com>
2021-09-06 14:11:19 +0100

    added homepage with login and sign up button

--- src/main/java/com/makersacademy/acebook/controller/HomeController.java
+++ src/main/java/com/makersacademy/acebook/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.makersacademy.acebook.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.view.RedirectView;
 
@@ -10,4 +11,14 @@ public class HomeController {
 	public RedirectView index() {
 		return new RedirectView("/posts");
 	}
+
+	@GetMapping("/homepage")
+	public String homepage() {
+		return "/homepage/index";
+	}
+
+//	@GetMapping("/login")
+//	public String newUser() {
+//		return "/login";
+//	}
 }
